8e8a83420fba43d756da9bca3b9c3ff453f8c054
Cindy <51303237+happycindy26@users.noreply.github.com>
2023-01-19 15:21:31 +1100

    create social media app

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+app.use(bodyParser.json({ limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(cors())
+// app.use(cors({
+//   origin: ['http://localhost:3000/', 'https://memories-mern-social-media-app.onrender.com']
+// }))
+
+app.use('/posts', postRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API')
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
+
+//mongoose.set('useFindAndModify', false)
+
+//https://www.mongodb.com/atlas/database
